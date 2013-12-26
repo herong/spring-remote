ce3b71de8d37e352f10cbe1afb31650a9ece50ec
herong <herong@herong.hnisi.com.cn>
2013-12-26 18:32:39 +0800

    【缺陷】解决spring配置中增加了spring-remote标签之后，导致NullPointException错误。原因：在spring配置文件中直接定义的bean会在spring-remote解析之后才实例化，spring-remote在解析时getBean时会抛NullPointException错误。

--- spring-remote-annotation/src/main/java/com/github/herong/spring/remoting/RemotingExporter.java
+++ spring-remote-annotation/src/main/java/com/github/herong/spring/remoting/RemotingExporter.java
@@ -2,6 +2,8 @@ package com.github.herong.spring.remoting;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.core.annotation.AnnotationUtils;
@@ -13,19 +15,27 @@ public class RemotingExporter implements BeanFactoryPostProcessor {
 
     public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
         for (String beanName : beanFactory.getBeanDefinitionNames()) {
-            Object bean = beanFactory.getBean(beanName);
-            Class<?> serviceInterface = findServiceInterface(bean);
+        	
+            BeanDefinition define = beanFactory.getBeanDefinition(beanName);
+            Class<?> cls = null;
+            try {
+				 cls = Class.forName(define.getBeanClassName(),false,beanFactory.getBeanClassLoader());
+			} catch (ClassNotFoundException e) {
+				throw new NoSuchBeanDefinitionException("RemotingExporter.postProcessBeanFactory at Loading bean class error!");
+			}
+            Class<?> serviceInterface = findServiceInterface(cls);
             if (serviceInterface != null) {
+            	Object bean = beanFactory.getBean(beanName);
                 RemoteExporter remoteExporter = createRemoteExporter(serviceInterface, bean);
                 beanFactory.registerSingleton("/" + serviceInterface.getName(), remoteExporter);
             }
         }
     }
 
-    private Class<?> findServiceInterface(Object service) {
+    private Class<?> findServiceInterface(Class<?> cls) {
         Class<?> serviceInterface = null;
-        if (AnnotationUtils.isAnnotationDeclaredLocally(Service.class, service.getClass())) {
-            for (Class<?> interfaceClass : service.getClass().getInterfaces()) {
+        if (AnnotationUtils.isAnnotationDeclaredLocally(Service.class, cls)) {
+            for (Class<?> interfaceClass : cls.getInterfaces()) {
                 if (AnnotationUtils.isAnnotationDeclaredLocally(Remote.class, interfaceClass)) {
                     serviceInterface = interfaceClass;
                 }
